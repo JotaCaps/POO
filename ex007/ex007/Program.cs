96dea99df222a0e8dbd18dfa1bdd9decd6315538
João Sérgio <sergiojoao494@gmail.com>
2024-06-25 10:28:50 -0300

    ex007

---
+++ ex007/ex007/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+string[] valores = Console.ReadLine().Split(' ');
+double x = double.Parse(valores[0], CultureInfo.InvariantCulture);
+double y = double.Parse(valores[1], CultureInfo.InvariantCulture);
+
+if (x == 0.0 && y == 0.0)
+{
+    Console.WriteLine("Origem");
+}
+else if (x == 0.0)
+{
+    Console.WriteLine("Eixo Y");
+}
+else if (y == 0.0)
+{
+    Console.WriteLine("Eixo X");
+}
+else if (x > 0.0 && y > 0.0)
+{
+    Console.WriteLine("Q1");
+}
+else if (x < 0.0 && y > 0.0)
+{
+    Console.WriteLine("Q2");
+}
+else if (x < 0.0 && y < 0.0)
+{
+    Console.WriteLine("Q3");
+}
+else
+{
+    Console.WriteLine("Q4");
+}
