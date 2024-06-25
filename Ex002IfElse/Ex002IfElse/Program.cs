8852ebac8b67f0bb73a851d6a7837e0f70b9f4d2
João Sérgio <sergiojoao494@gmail.com>
2024-06-25 09:34:26 -0300

    ex002

---
+++ Ex002IfElse/Ex002IfElse/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite um número: ");
+
+int n = int.Parse(Console.ReadLine());
+
+
+if (n % 2 == 0)
+{
+    Console.WriteLine("PAR!");
+}
+else
+{
+    Console.WriteLine("IMPAR!");
+}
--- ex002/ex002/Program.cs
+++ ex002/ex002/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite um número: ");
+double n = double.Parse(Console.ReadLine());
+
+if (n < 0)
+{
+    Console.WriteLine("NEGATIVO");
+}
+else
+{
+    Console.WriteLine("NÃO NEGATIVO");
+}
