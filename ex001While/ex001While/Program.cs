9f7176d7cc81b01f0c3ee5de75ea49e5dc0d1bf0
João Sérgio <sergiojoao494@gmail.com>
2024-06-27 13:54:41 -0300

    ex001While

---
+++ ex001While/ex001While/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Senha: ");
+int senha = int.Parse(Console.ReadLine());
+
+while (senha != 2002)
+{
+    Console.WriteLine("Senha Inválida!");
+    Console.WriteLine("Senha: ");
+    senha = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Acesso Permitido!");
