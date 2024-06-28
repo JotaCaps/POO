c8936ad7c4598d507e97f88f7a876c16a6f6215a
João Sérgio <sergiojoao494@gmail.com>
2024-06-28 12:09:33 -0300

    ex004

---
+++ ex004For/ex004For/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+
+int N = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    string[] line = Console.ReadLine().Split(' ');
+    int n1 = int.Parse(line[0]);
+    int n2 = int.Parse(line[1]);
+
+    if (n2 == 0)
+    {
+        Console.WriteLine("Divisão Impossível!");
+    }
+    else
+    {
+        double divisao = (double)n1 / n2; 
+        Console.WriteLine(divisao.ToString("F1", CultureInfo.InvariantCulture));
+    }
+}
