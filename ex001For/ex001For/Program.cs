0978fd03ebdde702d7b6a698e6979d852ea6d64e
João Sérgio <sergiojoao494@gmail.com>
2024-06-28 10:27:57 -0300

    ex001for

---
+++ ex001For/ex001For/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um número: ");
+int valor = int.Parse(Console.ReadLine());
+
+for (int i = 1;  i <= valor; i++)
+{
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+    
+}
