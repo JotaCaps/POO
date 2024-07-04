19bd03d2d83bbb4483a5aa78fe5b60aa74936c1e
João Sérgio <sergiojoao494@gmail.com>
2024-07-04 10:17:50 -0300

    ex001 Classes

---
+++ ex001Classes/ex001Classes/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex001Classes
+{
+    class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ ex001Classes/ex001Classes/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ex001Classes;
+
+Pessoa pessoa1, pessoa2;
+
+Console.WriteLine("Primeira pessoa: ");
+
+pessoa1 = new Pessoa();
+
+pessoa1.Nome = Console.ReadLine();
+pessoa1.Idade = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Segunda pessoa: ");
+
+pessoa2 = new Pessoa();
+
+pessoa2.Nome = Console.ReadLine();
+pessoa2.Idade = int.Parse(Console.ReadLine());
+
+if (pessoa1.Idade > pessoa2.Idade)
+{
+    Console.WriteLine($"{pessoa1.Nome} é mais velho(a)!");
+}
+else 
+{
+    Console.WriteLine($"{pessoa2.Nome} é mais velho(a)!");
+}
+
+
+
+
