3f0e05e746cb8515595b1ed6420a1abb97bb9186
João Sérgio <sergiojoao494@gmail.com>
2024-07-04 10:27:56 -0300

    ex002 classes

---
+++ ex002Classes/ex002Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ex002Classes;
+
+Funcionario funcionario1, funcionario2;
+
+Console.WriteLine("Primeiro funcionario:");
+
+funcionario1 = new Funcionario();
+
+funcionario1.Nome = Console.ReadLine();
+funcionario1.Salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Segundo funcionario:");
+
+funcionario2 = new Funcionario();
+
+funcionario2.Nome = Console.ReadLine();
+funcionario2.Salario = double.Parse(Console.ReadLine());
+
+double mediaSal = (funcionario1.Salario + funcionario2.Salario) / 2;
+
+Console.WriteLine(mediaSal);
+
