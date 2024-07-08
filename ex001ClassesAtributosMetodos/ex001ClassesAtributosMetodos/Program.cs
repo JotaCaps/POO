2e2034001922697a6d508ae0f509ec26a0a11f1b
João Sérgio <sergiojoao494@gmail.com>
2024-07-08 14:25:39 -0300

    Ex001 Classes Metodos e Atributos

---
+++ ex001ClassesAtributosMetodos/ex001ClassesAtributosMetodos/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+using ex001ClassesAtributosMetodos;
+
+Console.WriteLine("Entre com a largura e altura do retângulo: ");
+
+Retangulo r = new Retangulo();
+
+r.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+r.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("AREA: "+ r.Area().ToString("F2"), CultureInfo.InvariantCulture);
+Console.WriteLine($"PERÍMETRO: " + r.Perimetro().ToString("F2"), CultureInfo.InvariantCulture);
+Console.WriteLine($"DIAGONAL: " + r.Diagonal().ToString("F2"), CultureInfo.InvariantCulture);
+
---
+++ ex001ClassesAtributosMetodos/ex001ClassesAtributosMetodos/Retangulo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex001ClassesAtributosMetodos
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Sqrt(Largura) + Math.Sqrt(Altura));
+        }
+
+    }
+
+}
