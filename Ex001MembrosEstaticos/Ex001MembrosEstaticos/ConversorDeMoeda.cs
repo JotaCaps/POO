99b64a01d52e881b0a8d7f08451ae53d0a0855db
João Sérgio <sergiojoao494@gmail.com>
2024-07-11 14:46:04 -0300

    ExMembrosEstaticos

---
+++ Ex001MembrosEstaticos/Ex001MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex001MembrosEstaticos
+{
+    internal class ConversorDeMoeda
+    {
+        static double Iof = 0.6;
+
+        public static double Conversor(double d, double r)
+        {
+            double cotacao = d * r;
+            return cotacao + cotacao * 6 / 100;
+        }
+
+    }
+}
---
+++ Ex001MembrosEstaticos/Ex001MembrosEstaticos/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ex001MembrosEstaticos;
+using System.Globalization;
+
+Console.Write("Qual a cotação do Dólar? ");
+double ValorDolar = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Quantos doláres você vai comprar? :");
+double reais = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine("Valor a ser pago em reais: " + ConversorDeMoeda.Conversor(ValorDolar, reais).ToString("F2", CultureInfo.InvariantCulture));
