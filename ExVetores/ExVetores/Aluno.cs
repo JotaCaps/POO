21bced9b8e687c1b4f5e0a738ae21407a2e22c42
João Sérgio <sergiojoao494@gmail.com>
2024-07-17 16:20:11 -0300

    Ex Vetores

---
+++ ExVetores/ExVetores/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExVetores
+{
+    internal class Aluno
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Aluno(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
---
+++ ExVetores/ExVetores/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExVetores;
+using System.Globalization;
+using System.Numerics;
+
+Console.WriteLine("Quantos quartos serão alugados? ");
+int n = int.Parse(Console.ReadLine());
+
+Aluno[] vect = new Aluno[10];
+
+vect[2] = new Aluno("Jon", "Hotmail");
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Nome: ");
+    string nome = Console.ReadLine();
+    Console.Write("Email: ");
+    string email = Console.ReadLine();
+    Console.Write("Quarto: ");
+    int quarto = int.Parse(Console.ReadLine());
+    
+    if (vect[quarto] != null)
+    {
+        Console.WriteLine("Quarto Ocupado! Escolha outro: ");
+    }
+    else
+    {
+        vect[quarto] = new Aluno(nome, email);
+    }   
+}
+
+for (int i = 0; i < 10; i++)
+{
+    if (vect[i] != null)
+    {
+        Console.WriteLine(i + ": " + vect[i]);
+    }
+}
+
+
