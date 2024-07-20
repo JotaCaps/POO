266f2f41c8bdeb1d48757b511f91da4913b91f27
João Sérgio <sergiojoao494@gmail.com>
2024-07-20 15:30:51 -0300

    ex001 listas

---
+++ ExListas/ExListas/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExListas
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee() { }
+        public Employee(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void icreaseSalary(double percentage)
+        {
+            Salary += Salary * percentage / 100.0; 
+        }
+    }
+
+    
+}
---
+++ ExListas/ExListas/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+using System.Collections.Generic;
+using ExListas;
+
+Console.Write("How many employes will be registred? ");
+int r = int.Parse(Console.ReadLine());
+
+List<Employee> funcionarios = new List<Employee>();
+
+for (int i = 0; i < r; i++)
+{
+    Console.WriteLine("Employee #" + i + ":");
+
+    Console.WriteLine("ID: ");
+    int id = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Name: ");
+    string name = Console.ReadLine();
+
+    Console.WriteLine("Salary: ");
+    double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+    funcionarios.Add(new Employee(id, name, salary));
+    Console.WriteLine(" ");
+
+}
+
+Console.WriteLine("Enter the employee id that will have salary increase : ");
+int searchId = int.Parse(Console.ReadLine());
+
+Employee emp = funcionarios.Find(x => x.Id == searchId);
+
+if(emp != null)
+{
+    Console.Write("Enter the percentage: ");
+    double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    emp.icreaseSalary(percentage);
+}
+else
+{
+    Console.WriteLine("This id is does not exist!");
+}
+
+Console.WriteLine();
+Console.WriteLine("Updated list: ");
+
+
+foreach (Employee item in funcionarios)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
+
+
+
+
+
+
+
