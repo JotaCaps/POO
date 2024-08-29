b6e5eea19bed1727014b4d1af1f79cc91237e5c9
João Sérgio <sergiojoao494@gmail.com>
2024-08-29 18:37:30 -0300

    aula 1 sobre enum

---
+++ aula1-enum/aula1-enum/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using aula1_enum.Entities.Enums;
+using aula1_enum.Entities;
+using System;
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment.ToString();
+Console.WriteLine(txt);
+
+OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+Console.WriteLine(os);
