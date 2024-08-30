8525180fbe12299265fb5de80b22092dfb2ec67c
João Sérgio <sergiojoao494@gmail.com>
2024-08-30 19:17:12 -0300

    Programa finalizado

--- Exercicio1Enum/Exercicio1Enum/Etities/Department.cs
+++ Exercicio1Enum/Exercicio1Enum/Etities/Department.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Exercicio1Enum.Etities
 {
     internal class Department
     {
+        public string Name { get; set; }
+
+        public Department() { }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
     }
 }
--- Exercicio1Enum/Exercicio1Enum/Etities/Enums/WorkerLevel.cs
+++ Exercicio1Enum/Exercicio1Enum/Etities/Enums/WorkerLevel.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Exercicio1Enum.Etities.Enums
 {
-    internal class WorkerLevel
+    enum WorkerLevel : int
     {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
     }
 }
